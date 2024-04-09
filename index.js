29ab404de619f0f8b52c5fa0d05875dfc1e16e5b
ylmgrbzz <yalim.gurbuz@std.yildiz.edu.tr>
2024-04-09 22:18:55 +0300

    Add Redux store subscription and dispatch multiple buyCake actions

--- index.js
+++ index.js
@@ -28,3 +28,10 @@ const reducer = (state = initialState, action) => {
 
 const store = createStore(reducer);
 console.log("Initial state", store.getState());
+const unsubscribe = store.subscribe(() =>
+  console.log("Updated state", store.getState())
+);
+store.dispatch(buyCake());
+store.dispatch(buyCake());
+store.dispatch(buyCake());
+unsubscribe();
