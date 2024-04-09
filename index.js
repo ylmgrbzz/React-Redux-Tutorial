f9618c31477ef68ce10c5ee424831d685466cd24
ylmgrbzz <yalim.gurbuz@std.yildiz.edu.tr>
2024-04-09 22:28:28 +0300

    Add Redux combineReducers and create rootReducer

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const redux = require("redux");
 const createStore = redux.createStore;
+const combineReducers = redux.combineReducers;
 
 const BUY_CAKE = "BUY_CAKE";
 const BUY_ICECREAM = "BUY_ICECREAM";
@@ -52,3 +53,44 @@ store.dispatch(buyIceCream());
 store.dispatch(buyIceCream());
 store.dispatch(buyIceCream());
 unsubscribe();
+
+const cakeReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case BUY_CAKE:
+      return {
+        ...state,
+        numOfCakes: state.numOfCakes - 1,
+      };
+    default:
+      return state;
+  }
+};
+
+const iceCreamReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case BUY_ICECREAM:
+      return {
+        ...state,
+        numOfIceCreams: state.numOfIceCreams - 1,
+      };
+    default:
+      return state;
+  }
+};
+
+const rootReducer = combineReducers({
+  cake: cakeReducer,
+  iceCream: iceCreamReducer,
+});
+
+const store2 = createStore(rootReducer);
+console.log("Initial state2", store2.getState());
+const unsubscribe2 = store2.subscribe(() =>
+  console.log("Updated state2", store2.getState())
+);
+store2.dispatch(buyCake());
+store2.dispatch(buyCake());
+store2.dispatch(buyCake());
+store2.dispatch(buyIceCream());
+
+unsubscribe2();
