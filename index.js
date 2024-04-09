e74baea8c91bd4b660ece0728fa41282527fb105
ylmgrbzz <yalim.gurbuz@std.yildiz.edu.tr>
2024-04-09 22:15:19 +0300

    Add Redux store and initial state logging

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+const redux = require("redux");
+const createStore = redux.createStore;
+
 const BUY_CAKE = "BUY_CAKE";
 
 function buyCake() {
@@ -22,3 +25,6 @@ const reducer = (state = initialState, action) => {
       return state;
   }
 };
+
+const store = createStore(reducer);
+console.log("Initial state", store.getState());
