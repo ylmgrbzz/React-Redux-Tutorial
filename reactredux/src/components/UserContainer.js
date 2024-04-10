3a868128d9f37b2dd714e169b29af2a59111e5ad
ylmgrbzz <yalim.gurbuz@std.yildiz.edu.tr>
2024-04-10 17:16:19 +0300

    Update UserContainer to connect with Redux store

--- reactredux/src/components/UserContainer.js
+++ reactredux/src/components/UserContainer.js
@@ -1,7 +1,21 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { connect } from "react-redux";
+import { fetchUsers } from "../redux";
+
+const mapStateToProps = (state) => {
+  return {
+    userData: state.user,
+  };
+};
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    fetchUsers: () => dispatch(fetchUsers()),
+  };
+};
 
 const UserContainer = () => {
   return <div>UserContainer</div>;
 };
 
-export default UserContainer;
+export default connect(mapStateToProps, mapDispatchToProps)(UserContainer);
