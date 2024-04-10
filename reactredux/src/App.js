3ce15d5f2b1b94ef712cc7f5e0f21410141523cf
ylmgrbzz <yalim.gurbuz@std.yildiz.edu.tr>
2024-04-10 17:25:08 +0300

    Add UserContainer component to App.js and connect with Redux store

--- reactredux/src/App.js
+++ reactredux/src/App.js
@@ -5,6 +5,8 @@ import { Provider } from "react-redux";
 import store from "./redux/store";
 import IceCreamContainer from "./components/IceCreamContainer";
 import NewCakeContainer from "./components/NewCakeContainer";
+import UserContainer from "./components/UserContainer";
+import HooksCakeContainer from "./components/HooksCakeContainer";
 
 function App() {
   return (
@@ -13,6 +15,8 @@ function App() {
         <CakeContaier />
         <IceCreamContainer />
         <NewCakeContainer />
+        <HooksCakeContainer />
+        <UserContainer />
       </div>
     </Provider>
   );
