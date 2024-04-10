a3ee8917ab84280fd9101484cb294cac7e3773e9
ylmgrbzz <yalim.gurbuz@std.yildiz.edu.tr>
2024-04-10 17:14:16 +0300

    Add axios and redux-thunk dependencies

--- reactredux/src/redux/user/userActions.js
+++ reactredux/src/redux/user/userActions.js
@@ -1,3 +1,4 @@
+import axios from "axios";
 import {
   FETCH_USERS_REQUEST,
   FETCH_USERS_SUCCESS,
@@ -19,5 +20,20 @@ export const fethUsersFailure = (error) => {
   return {
     type: FETCH_USERS_FAILURE,
     payload: error,
+  };
+};
+
+export const fetchUser = () => {
+  return (dispatch) => {
+    dispatch(fethUsersRequest());
+    axios
+      .get("https://jsonplaceholder.typicode.com/users")
+      .then((response) => {
+        const users = response.data;
+        dispatch(fethUsersSuccess(users));
+      })
+      .catch((error) => {
+        dispatch(fethUsersFailure(error));
+      });
   };
 };
