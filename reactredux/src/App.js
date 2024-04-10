ffd3bff2f11819fdaa7b22aaf7708a402a1903bb
ylmgrbzz <yalim.gurbuz@std.yildiz.edu.tr>
2024-04-10 14:29:12 +0300

    Add Redux Provider component to App.js

--- reactredux/src/App.js
+++ reactredux/src/App.js
@@ -1,12 +1,16 @@
 import logo from "./logo.svg";
 import "./App.css";
 import CakeContaier from "./components/CakeContaier";
+import { Provider } from "react-redux";
+import store from "./redux/store";
 
 function App() {
   return (
-    <div className="App">
-      <CakeContaier />
-    </div>
+    <Provider store={store}>
+      <div className="App">
+        <CakeContaier />
+      </div>
+    </Provider>
   );
 }
 
