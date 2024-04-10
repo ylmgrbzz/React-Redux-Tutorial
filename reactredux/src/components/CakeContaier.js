7172bf114ca7446808cf1d6b3d82c90916d10de0
ylmgrbzz <yalim.gurbuz@std.yildiz.edu.tr>
2024-04-10 14:34:58 +0300

    Update CakeContaier.js to use Redux connect function for state and dispatch mapping

--- reactredux/src/components/CakeContaier.js
+++ reactredux/src/components/CakeContaier.js
@@ -1,12 +1,26 @@
 import React from "react";
+import { buyCake } from "../redux";
+import { connect } from "react-redux";
 
-const CakeContaier = () => {
+const CakeContaier = (props) => {
   return (
     <div>
-      <h2>Number of cakes</h2>
-      <button>Buy Cake</button>
+      <h2>Number of cakes {props.numOfCakes} </h2>
+      <button onClick={props.buyCake}>Buy Cake</button>
     </div>
   );
 };
 
-export default CakeContaier;
+const mapStateToProps = (state) => {
+  return {
+    numOfCakes: state.numOfCakes,
+  };
+};
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    buyCake: () => dispatch(buyCake()),
+  };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(CakeContaier);
