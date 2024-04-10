b8b320511c8d93cfb271fd0fa3790d38c7f21eaf
ylmgrbzz <yalim.gurbuz@std.yildiz.edu.tr>
2024-04-10 14:25:11 +0300

    Add new files and components for React Redux setup

---
+++ reactredux/src/App.js
@@ -0,0 +1,13 @@
+import logo from "./logo.svg";
+import "./App.css";
+import CakeContaier from "./components/CakeContaier";
+
+function App() {
+  return (
+    <div className="App">
+      <CakeContaier />
+    </div>
+  );
+}
+
+export default App;
---
+++ reactredux/src/components/CakeContaier.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const CakeContaier = () => {
+  return (
+    <div>
+      <h2>Number of cakes</h2>
+      <button>Buy Cake</button>
+    </div>
+  );
+};
+
+export default CakeContaier;
