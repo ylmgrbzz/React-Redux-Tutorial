16485d8e40bad89b03d2e28863a47c7624936566
ylmgrbzz <yalim.gurbuz@std.yildiz.edu.tr>
2024-04-10 16:11:24 +0300

    Add userReducer to rootReducer.js

--- reactredux/src/redux/rootReducer.js
+++ reactredux/src/redux/rootReducer.js
@@ -1,10 +1,11 @@
 import { combineReducers } from "redux";
-
 import cakeReducer from "./cakes/CakeReducer";
 import iceCreamReducer from "./iceCream/iceCreamReducer";
+import userReducer from "./user/userReducer";
 
 const rootReducer = combineReducers({
   cake: cakeReducer,
+  user: userReducer,
   iceCream: iceCreamReducer,
 });
 
