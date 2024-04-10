e2388a46210504287517ef1f24bd5ce10a2524b8
ylmgrbzz <yalim.gurbuz@std.yildiz.edu.tr>
2024-04-10 14:26:32 +0300

    Update CakeActions.js to export buyCake function

---
+++ reactredux/src/redux/cakes/CakeReducer.js
@@ -0,0 +1,15 @@
+const initialState = {
+  numOfCakes: 10,
+};
+
+const cakeReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case "BUY_CAKE":
+      return {
+        ...state,
+        numOfCakes: state.numOfCakes - 1,
+      };
+    default:
+      return state;
+  }
+};
